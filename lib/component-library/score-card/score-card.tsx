2bf5708bc03356934423f530a21a2f90874d88e5
Reece <reecemorgandev@gmail.com>
2024-04-03 18:57:37 +0100

    styled score card

--- lib/component-library/score-card/score-card.tsx
+++ lib/component-library/score-card/score-card.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { quizResults } from '@types';
+import styled from 'styled-components';
 
 interface Props {
     quizResult: quizResults;
@@ -14,44 +15,67 @@ export const ScoreCard = ({ quizResult, questions, name }: Props) => {
     const status = percentage >= passPercentage ? 'Pass' : 'Fail';
  
     return (
-        <>
-            <div>
-                <h3>Hello, {name}. Here is your Result Analysis</h3>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>Total Questions:</td>
-                            <td>{questions.length}</td>
-                        </tr>
-                        <tr>
-                            <td>Total Score:</td>
-                            <td>{quizResult.score}</td>
-                        </tr>
-                        <tr>
-                            <td>Correct Answers:</td>
-                            <td>{quizResult.correctAnswers}</td>
-                        </tr>
-                        <tr>
-                            <td>Wrong Answers:</td>
-                            <td>{quizResult.wrongAnswers}</td>
-                        </tr>
-                        <tr>
-                            <td>Percentage:</td>
-                            <td>{percentage}%</td>
-                        </tr>
-                        <tr>
-                            <td>Status:</td>
-                            <td>{status}</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <button
-                    onClick={() => window.location.reload()}
-                    className='btn btn-primary mt-3'
-                >
-                    Restart
-                </button>
-            </div>
-        </>
+        <Wrapper>
+            <Title>Hello, <Span>{name}</Span>. Here is your Result Analysis</Title>
+            <Table>
+                <TableBody>
+                    <TableRow>
+                        <TableData>Total Questions:</TableData>
+                        <TableData>{questions.length}</TableData>
+                    </TableRow>
+                    <TableRow>
+                        <TableData>Total Score:</TableData>
+                        <TableData>{quizResult.score}</TableData>
+                    </TableRow>
+                    <TableRow>
+                        <TableData>Correct Answers:</TableData>
+                        <TableData>{quizResult.correctAnswers}</TableData>
+                    </TableRow>
+                    <TableRow>
+                        <TableData>Wrong Answers:</TableData>
+                        <TableData>{quizResult.wrongAnswers}</TableData>
+                    </TableRow>
+                    <TableRow>
+                        <TableData>Percentage:</TableData>
+                        <TableData>{percentage}%</TableData>
+                    </TableRow>
+                    <TableRow>
+                        <TableData>Status:</TableData>
+                        <TableData>{status}</TableData>
+                    </TableRow>
+                </TableBody>
+            </Table>
+            <Button onClick={() => window.location.reload()}>
+                Restart
+            </Button>
+        </Wrapper>
     );
 };
+
+const Wrapper = styled.div``;
+
+const Title = styled.p`
+  font-weight: 600;
+  font-size: 1.2em;
+`;
+
+const Span = styled.span`
+  text-transform: capitalize;
+`;
+
+const Table = styled.table``;
+
+const TableBody = styled.tbody``;
+
+const TableRow = styled.tr``;
+
+const TableData = styled.td``;
+
+const Button = styled.button`
+  color: #fff;
+  background-color: #023610;
+  cursor: pointer;
+  border: 1px solid #fff;
+  border-radius: 5px;
+  font-size: 1em;
+`;

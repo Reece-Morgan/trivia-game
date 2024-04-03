b02791824c47be31fc1df5012393e2612c7d5528
Reece <reecemorgandev@gmail.com>
2024-04-03 18:25:21 +0100

    added Quiz to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,68 @@
 'use client'
 import styled from "styled-components";
+import { useState } from "react";
+import { Quiz } from "@component-library";
+import { breakpointValues } from "@settings";
 
 export default function Home() {
-  return <Wrapper>{/* TODO: Add content here */}</Wrapper>;
+  const [quizStarted, setQuizStarted] = useState<boolean>(false);
+  const [name, setName] = useState<string>('');
+
+  return (
+    <Wrapper>
+      {quizStarted ? (
+          <Quiz name={name} />
+      ) : (
+          <FormWrapper>
+              <Form>
+                  <Label htmlFor="nameInput">
+                      Enter Your Name:
+                  </Label>
+                  <Input
+                      type="text"
+                      id="nameInput"
+                      value={name}
+                      onChange={(e) =>
+                          setName(e.target.value)}
+                  />
+              </Form>
+              <Button
+                  onClick={() => setQuizStarted(true)}
+                  disabled={!name.trim()}
+              >
+                  Start Quiz
+              </Button>
+          </FormWrapper>
+      )}
+    </Wrapper>
+  )
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  max-width: ${breakpointValues.lg};
+  margin: 0 auto;
+`;
+
+const FormWrapper = styled.div`
+  color: #fff;
+`;
+
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px 0;
+`;
+
+const Label = styled.label``;
+
+const Input = styled.input`
+  max-width: 100px;
+  margin: 10px 0;
+`;
+
+const Button = styled.button<{disabled: boolean}>`
+  color: #000;
+  border: 2px solid #fff;
+  background-color: ${props => props.disabled ? '#d3d3d3' : '#0f0'};
+  cursor: ${props => props.disabled ? 'not-allowed' : 'pointer'};
+`;
--- lib/component-library/quiz/quiz.tsx
+++ lib/component-library/quiz/quiz.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { quiz } from '@data';
 import { quizResults } from '@types';
 import { ScoreCard } from '../score-card/score-card';

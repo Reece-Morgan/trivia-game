6252e9eb3b1b9032afe224a5682c7e29f13a9133
Reece <reecemorgandev@gmail.com>
2024-04-03 21:42:14 +0100

    added temporary quiz

---
+++ lib/data/super-duper-hard-jakey-d-quiz.ts
@@ -0,0 +1,41 @@
+import { questionData } from "../types";
+
+export const superDuperJakeyDFootballQuiz: questionData = {
+    questions: [
+        {
+            id: 1,
+            question: 'Who is the only player to have won the UEFA Champions League with three different clubs?',
+            answers: ['Cristiano Ronaldo',
+                'Clarence Seedorf',
+                'Xavi Hernández',
+                'Andrés Iniesta'],
+            correctAnswer: 'Clarence Seedorf',
+        },
+        {
+            id: 2,
+            question: `Which country won the first ever FIFA World Cup in 1930?`,
+            answers: ['Italy', 'Uruguay', 'Argentina', 'Brazil'],
+            correctAnswer: 'Uruguay',
+        },
+        {
+            id: 3,
+            question: `Which goalkeeper has the most clean sheets in the history of the English Premier League?`,
+            answers: ['Peter Schmeichel', 'Petr Čech', 'David de Gea', 'Edwin van der Sar'],
+            correctAnswer: 'Petr Čech',
+        },
+        {
+            id: 4,
+            question: 'Who is the only player to have won the FIFA World Cup, UEFA European Championship, and UEFA Champions League?',
+            answers: ['Cristiano Ronaldo', 'Philipp Lahm',
+                'Thierry Henry', 'Xavi Hernández'],
+            correctAnswer: 'Philipp Lahm',
+        },
+        {
+            id: 5,
+            question: 'Which club won the first edition of the FIFA Club World Cup in 2000?',
+            answers: ['Boca Juniors', 'Corinthians',
+                'Real Madrid', 'Manchester United'],
+            correctAnswer: 'Corinthians',
+        },
+    ],
+};
